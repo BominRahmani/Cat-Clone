1551a7b224114305e3087eb6ded6985b8a3b7ebb
OzymandiasB <bominrahmani@gmail.com>
2022-01-24 16:06:55 -0500

    prelim stuff

---
+++ cmake-build-debug/CMakeFiles/lol.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lol.dir/main.cpp.o"
+  "lol"
+  "lol.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lol.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, char *argv[])
+{
+    using namespace std;
+
+    // make sure there is input going in
+    if (argc < 2) {
+    cout << argc << endl;
+    cout << argv[1] << endl;
+    }
+    
+
+    return 0;
+}
