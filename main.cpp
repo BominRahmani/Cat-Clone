337d5e2e27cb0bda4d9f46903006d07a961d2602
OzymandiasB <bominrahmani@gmail.com>
2022-02-01 18:21:37 -0500

    -r and -s implemeneted

--- main.cpp
+++ main.cpp
@@ -31,55 +31,90 @@ vector<char> readBinary(string filename) {
 /**
  * Check to see if file is binary type
  */
-bool translateBinary(vector <char> preFiltered) {
+bool translateBinary(vector <char> preFiltered, bool sFlag, bool rFlag) {
     vector <string> filteredData;
+    vector <string> reversedData;
+    string runningLine;
     string temp;
     // go through and start changing ascii into their filtered form
     for (int i = 0; i < preFiltered.size(); i++) {
         temp = preFiltered[i];
         int charNum = static_cast<int>(static_cast<unsigned char>(preFiltered[i]));
         unsigned char modifier = (static_cast<unsigned char>(preFiltered[i]));
-        //cout << "char num for this number is: " << charNum << endl;
-        //cout << static_cast<int>(static_cast<unsigned char>(preFiltered[i])) << endl;
         if ((charNum > -1 && charNum < 32)){
             temp = (char) (static_cast<int> (modifier + 64));
-            //temp = static_cast<unsigned char>(static_cast<int>(preFiltered[i]) + 64);
             if (temp == "J"){
                 filteredData.push_back("\n");
+                runningLine += "\n";
+                reversedData.push_back(runningLine);
+                runningLine = "";
             }
             else if(temp == "I"){
                 filteredData.push_back("\t");
+                runningLine += "\t";
             }
             else {
                 filteredData.push_back("^" + temp);
+                runningLine += "^" + temp;
             }
         }
         else if (charNum == 127) {
             temp = (char) (static_cast<int>(modifier - 64));
             filteredData.push_back("^" + temp);
+            runningLine += "^" + temp;
         }
         else if (charNum > 127 && charNum < 160) {
             // extra printable char at end is - 64
             temp = (char) (static_cast<int>(modifier - 64));
             filteredData.push_back("M-^" + temp);
+            runningLine += "M-^" + temp;
         }
         else if (charNum == 255){
             temp = (char) (static_cast<int>(modifier - 128));
             filteredData.push_back("M-^" + temp);
+            runningLine += "M-^" + temp;
         }
         else if ( charNum >= 160) {
             temp = (char) (static_cast<int>(static_cast<unsigned char>(preFiltered[i])) - 128);
             filteredData.push_back("M-" + temp);
+            runningLine += "M-" + temp;
         }
         else {
             filteredData.push_back(temp);
+            runningLine += temp;
         }
-        //cout << static_cast<int>(preFiltered[i]) << endl;
     }
-    for (int i = 0; i < filteredData.size(); i++){
-        cout << filteredData[i];
+
+    if (sFlag){
+        string peakChar;
+        for (int i = 0; i < filteredData.size(); i++){
+            if (filteredData[i] == "\n" && (i + 1 < filteredData.size()) ){
+                peakChar = filteredData[i + 1];
+                if (peakChar == "\n"){
+                    for(int x = i + 1; x < filteredData.size(); ++x){
+                        if(filteredData[x] != "\n"){
+                            // delete the subsequent \n's
+                            filteredData.erase(filteredData.begin() + i + 1, filteredData.begin() + x - 1);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
     }
-    
+
+    if(rFlag){
+        for (int i = filteredData.size() - 1; i >= 0; i--){
+            cout << reversedData[i];
+        }
+    }
+
+    else{
+        for (int i = 0; i < filteredData.size(); i++){
+            cout << filteredData[i];
+        }
+    }
+
     return false;
 }
 
@@ -96,6 +131,8 @@ int main(int argc, char *argv[])
     bool vFlag = false;
     bool eFlag = false;
     bool nFlag= false;
+    bool sFlag = false;
+    bool rFlag = false;
 
     // parse arguments to set flag validity
     for (int i = 1; i < argc; ++i){
@@ -111,6 +148,12 @@ int main(int argc, char *argv[])
             if(argumentFlag.find("v") != string::npos){
                 vFlag = true;
             }
+            if(argumentFlag.find("s") != string::npos){
+                sFlag = true;
+            }
+            if(argumentFlag.find("r") != string::npos){
+                rFlag = true;
+            }
         }
     }
     // find position of files and modify offset
@@ -124,7 +167,7 @@ int main(int argc, char *argv[])
     fileCount++;
     // if binary flags are enabled, use binary
     if (vFlag || eFlag) {
-        translateBinary(readBinary(fileInput));
+        translateBinary(readBinary(fileInput), sFlag, rFlag);
     }
     else {
         int lineCount = 1;
@@ -137,7 +180,7 @@ int main(int argc, char *argv[])
                 if (nFlag){
                     nFlagModifier = "\t" + to_string(lineCount) + "  ";
                     }
-                cout << nFlagModifier + line << "\n";
+                cout << nFlagModifier + line;
                 lineCount++;
             }
             myFile.close();
